3d3ae144a4dfa123c67e98dce831bb1198c02b50
sachit <ranasachit2001jan@gmail.com>
2023-10-12 15:32:17 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('connected to MangoDB')
+}).catch((err) => {
+    console.log(err)
+});
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('server listening on port 3000!')
-})
+  console.log('server listening on port 3000!');
+});
