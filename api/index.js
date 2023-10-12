2a6d4f3549e375893d7a5eb10542fc74f50fdca0
sachit <ranasachit2001jan@gmail.com>
2023-10-12 18:05:59 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import useRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.routes.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('connected to MangoDB')
@@ -10,6 +12,11 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('server listening on port 3000!');
 });
+
+app.use('/api/user', useRoutes)
+app.use('/api/auth', authRoutes);
